b4bc730112a49f6cc7ba9e6f0ab768176f5ee0e1
s19317 <61834247+s19317@users.noreply.github.com>
2020-03-19 15:35:31 +0200

    Start of program
    new

---
+++ cw2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace cw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //https://github.com/s19317/cw2.git
+        }
+    }
+}
